e464dd5f478ba1989d96ff19d8704b4330405280
= <saleh.majeed8@gmail.com>
2020-12-05 18:01:24 +0530

    character classes

--- regex.js
+++ regex.js
@@ -29,15 +29,30 @@
 
 /**
  *  Meta Character->
+ *
+ *  Single Characters ->
  *      \d => 0-9.
  *      \w => a-zA-z0-9 character.
  *      \s => (space, tab, line-break).
  *      . => any character.
+ *
  *  Quantifier ->
  *      * => 0 or more.
  *      + => 1 or more.
  *      ? => 0 or one.
  *      {} => limit.
  *
- *  Position
+ * Position ->
+ *      ^ -> begining.
+ *      $ -> ending.
+ *      \b -> boundry.
+ *
+ * Character Classes ->
+ *      [^str] -> anything except str.
+ *      () => know as group and by
+ *      $0 -> whole group.
+ *      $1 ... -> specific group and can be replace.
+ *
+ * Back References ->
+ *
  */
